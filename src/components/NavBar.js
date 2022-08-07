2ffeda583adb7f348e11583c3f609b3c8511264d
d-mini <diem-mi.nguyen@stud.hs-hannover.de>
2022-08-07 17:11:43 +0200

    ADD burger icon opens menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import { HashLink } from 'react-router-hash-link';
 import burgerMenu from '../images/Hamburger_icon.svg';
 import logo from '../images/logo.svg';
 
 function NavBar(props) {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   const offsetScroll = (element) => {
     const offset = 130;
     const bodyRect = document.body.getBoundingClientRect().top;
@@ -20,10 +22,12 @@ function NavBar(props) {
 
   return (
     <nav className={`navbar navbar-${props.color}`}>
-      <button className='burger-menu'>
+      <button className='burger-menu' onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
         <img src={burgerMenu} alt='Menu'/>
       </button>
-      <ul>
+      <ul className={ isNavExpanded ? "nav-menu-expanded" : "nav-menu" }>
         <li><HashLink className={'navlink' + (props.activeSection === 0 ? ' navlink-active' : '')} smooth to='/#'>Home</HashLink></li>
         <li><HashLink className={'navlink' + (props.activeSection === 1 ? ' navlink-active' : '')} smooth scroll={offsetScroll} to='/#section1'>Ingredients</HashLink></li>
         <li className='logo'><HashLink smooth to='/#'><img src={logo} alt="Logo"/></HashLink></li>
