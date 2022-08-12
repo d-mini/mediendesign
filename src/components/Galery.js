fdc70c13a9f9d94629f2690ed7495b401bf7fe8b
d-mini <diem-mi.nguyen@stud.hs-hannover.de>
2022-08-12 17:35:12 +0200

    UPDATE styling

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -2,16 +2,16 @@ import React from 'react';
 import GaleryImage from './GaleryImage';
 import './Galery.css';
 import porkBelly from '../images/pork_belly.jpg';
-import kimchi from '../images/kimchi.jpg'
+import kimchi from '../images/kimchi.jpg';
 
 
 function Galery() {
   return (
     <div className='galery'>
-      <GaleryImage src={porkBelly} label='Pork Belly'></GaleryImage>
-      <GaleryImage src={kimchi} label='Kimchi'></GaleryImage>
-      <GaleryImage src={porkBelly} label='Pork Belly'></GaleryImage>
-      <GaleryImage src={kimchi} label='Kimchi'></GaleryImage>
+      <GaleryImage src={'../images/pork_belly.jpg'} label='Pork Belly'></GaleryImage>
+      <GaleryImage src={'../images/kimchi.jpg'} label='Kimchi'></GaleryImage>
+      <GaleryImage src={'../images/pork_belly.jpg'} label='Pork Belly'></GaleryImage>
+      <GaleryImage src={'../images/kimchi.jpg'} label='Kimchi'></GaleryImage>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './NavBar.css';
 import { HashLink } from 'react-router-hash-link';
-import logo from '../images/logo.svg';
 
 function NavBar(props) {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
@@ -31,7 +30,17 @@ function NavBar(props) {
         </svg>
       </button>
       <ul className={ isNavExpanded ? "nav-menu-expanded" : "nav-menu" }>
-        <li className='logo'><HashLink smooth to='/#'><img src={logo} alt="Logo"/></HashLink></li>
+        <li className='logo'>
+          <HashLink smooth to='/#'>
+            <svg viewBox="0 0 1024 512" xmlns="http://www.w3.org/2000/svg">
+              <g fill='#fff'>
+                <text x="83.533px" y="363.666px" style={{ fontFamily:'NanumMyeongjo, Nanum Myeongjo, serif', fontSize:'300px' }}>삼겹살</text>
+                <path d="M1024,49.072l-49.072,-49.072l-925.856,0l-49.072,49.072l0,413.856l49.072,49.072l925.856,0l49.072,-49.072l-0,-413.856Z" style={{ fillOpacity:0 }}/>
+                <path d="M1024,49.072l-49.072,-49.072l-925.856,0l-49.072,49.072l0,413.856l49.072,49.072l925.856,0l49.072,-49.072l-0,-413.856Zm-20.833,8.629l-0,396.598c-0,-0 -36.868,36.868 -36.868,36.868c-0,-0 -908.598,-0 -908.598,-0c0,-0 -36.868,-36.868 -36.868,-36.868c0,-0 0,-396.598 0,-396.598c0,0 36.868,-36.868 36.868,-36.868c0,0 908.598,0 908.598,0l36.868,36.868Z"/>
+              </g>
+            </svg>
+          </HashLink>
+        </li>
         <li className='navitem'><HashLink className={'navlink' + (props.activeSection === 0 ? ' navlink-active' : '')} smooth to='/#'>Home</HashLink></li>
         <li className='navitem'><HashLink className={'navlink' + (props.activeSection === 1 ? ' navlink-active' : '')} smooth scroll={offsetScroll} to='/#section1'>About</HashLink></li>
         <li className='navitem'><HashLink className={'navlink' + (props.activeSection === 2 ? ' navlink-active' : '')} smooth scroll={offsetScroll} to='/#section2'>Ingredients</HashLink></li>
