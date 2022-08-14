90796e3982ccf1f6c8541a482a059efc4bba9a54
d-mini <diem-mi.nguyen@stud.hs-hannover.de>
2022-08-14 14:02:35 +0200

    FIX index.css not considered

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
+import './main.css';
 import './index.css';
-import "./main.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
