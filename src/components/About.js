56cfe7f83e03bc43b6ad525a275a894ba5e4740c
d-mini <diem-mi.nguyen@stud.hs-hannover.de>
2022-08-14 23:47:59 +0200

    UPDATE main.css

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ function About() {
                             <span>sal - meat</span>
                         </div>
 
-                        <div className='mt-4 text-center lg:text-left text-sm'>- Meaning of 삼겹살</div>
+                        <div className='mt-4 italic text-center lg:text-left text-sm'>- Meaning of 삼겹살</div>
                     </div>
                 </div>
             </div>
